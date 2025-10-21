f6697b8498c9a58900d4a677ce55c8b6735510ad
suhail ashraf <suhailashraf282@gmail.com>
2025-10-21 16:43:00 +0530

    eda and model

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 
 import sys
 import logging
+from src.logger import logging
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,3 +21,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.info("An exception occurred")
+#         raise CustomException(e, sys)
