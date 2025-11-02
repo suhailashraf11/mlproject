5d101d471f72330eaeea1874768321747cacedbe
suhail ashraf <suhailashraf282@gmail.com>
2025-11-02 22:58:52 +0530

    New add

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
-
+from src.components.data_transformation import DataTransformation 
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -19,8 +20,6 @@ class DataIngestionConfig:
     test_data_path: str = os.path.join('artifacts', 'test.csv')
 
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
-
-
 
 
 class DataIngestion:
@@ -58,5 +57,13 @@ class DataIngestion:
         
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
+
+
 
